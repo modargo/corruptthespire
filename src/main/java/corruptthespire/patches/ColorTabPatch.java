7f10174e4184dfe52e1c2a59164373a222d87d35
modargo <modargo@gmail.com>
2021-08-26 15:48:57 -0400

    Make Corrupted color name show up properly in the card library

---
+++ src/main/java/corruptthespire/patches/ColorTabPatch.java
@@ -0,0 +1,37 @@
+package corruptthespire.patches;
+
+import basemod.ReflectionHacks;
+import basemod.patches.com.megacrit.cardcrawl.screens.mainMenu.ColorTabBar.ColorTabBarFix;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.evacipated.cardcrawl.modthespire.lib.*;
+import com.evacipated.cardcrawl.modthespire.patcher.PatchingException;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.helpers.FontHelper;
+import com.megacrit.cardcrawl.localization.UIStrings;
+import com.megacrit.cardcrawl.screens.mainMenu.ColorTabBar;
+import corruptthespire.cards.corrupted.CorruptedCardColor;
+import javassist.CannotCompileException;
+import javassist.CtBehavior;
+
+import java.util.ArrayList;
+
+@SpirePatch(clz = ColorTabBarFix.Render.class, method = "Insert")
+public class ColorTabPatch {
+    private static final String[] TEXT = CardCrawlGame.languagePack.getUIString("CorruptTheSpire:CorruptedColor").TEXT;
+
+    @SpireInsertPatch(locator = Locator.class)
+    public static void Insert(ColorTabBar __instance, SpriteBatch sb, float y, ColorTabBar.CurrentTab curTab, @ByRef String[] ___tabName, int ___i) {
+        ArrayList<ColorTabBarFix.ModColorTab> modTabs = ReflectionHacks.getPrivateStatic(ColorTabBarFix.Fields.class, "modTabs");
+        if (modTabs.get(___i).color.equals(CorruptedCardColor.CORRUPTTHESPIRE_CORRUPTED)) {
+            ___tabName[0] = TEXT[0];
+        }
+    }
+
+    public static class Locator extends SpireInsertLocator {
+        public int[] Locate(CtBehavior ctMethodToPatch) throws CannotCompileException, PatchingException {
+            Matcher matcher = new Matcher.MethodCallMatcher(FontHelper.class, "renderFontCentered");
+            return LineFinder.findInOrder(ctMethodToPatch, matcher);
+        }
+
+    }
+}
