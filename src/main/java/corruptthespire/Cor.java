c4949a781c1af8a01b6cfccf1959becd7dafba96
modargo <modargo@gmail.com>
2021-08-26 22:40:00 -0400

    Add Corrupted Omamori relic

--- src/main/java/corruptthespire/Cor.java
+++ src/main/java/corruptthespire/Cor.java
@@ -115,6 +115,7 @@ public class Cor {
         corruptedRelics.add(new BagOfTricks());
         corruptedRelics.add(new BlightedSpirefruit());
         corruptedRelics.add(new CorruptedEgg());
+        corruptedRelics.add(new CorruptedOmamori());
         corruptedRelics.add(new MaskOfNightmares());
         corruptedRelics.add(new ObsidianShovel());
         corruptedRelics.add(new RustedOrichalcum());
