b4934c282b24e952b00c59b4f11dfe8aaaac74ac
modargo <modargo@gmail.com>
2021-08-31 11:51:23 -0400

    Add Corrupted Shrine event

--- src/main/java/corruptthespire/events/CorruptedEventUtil.java
+++ src/main/java/corruptthespire/events/CorruptedEventUtil.java
@@ -19,6 +19,7 @@ public class CorruptedEventUtil {
         corruptedEvents.put(WithoutBeginningOrEnd.ID, new CorruptedEventInfo(WithoutBeginningOrEnd.class, CorruptedEventType.Chaotic));
         corruptedEvents.put(WorldsUponWorlds.ID, new CorruptedEventInfo(WorldsUponWorlds.class, CorruptedEventType.Chaotic));
         corruptedEvents.put(AncientLaboratory.ID, new CorruptedEventInfo(AncientLaboratory.class, CorruptedEventType.Corrupted));
+        corruptedEvents.put(CorruptedShrine.ID, new CorruptedEventInfo(CorruptedShrine.class, CorruptedEventType.Corrupted));
         corruptedEvents.put(ForbiddenLibrary.ID, new CorruptedEventInfo(ForbiddenLibrary.class, CorruptedEventType.Corrupted));
         corruptedEvents.put(IncantationOfCorruption.ID, new CorruptedEventInfo(IncantationOfCorruption.class, CorruptedEventType.Corrupted));
         corruptedEvents.put(NightmareBloom.ID, new CorruptedEventInfo(NightmareBloom.class, CorruptedEventType.Corrupted));
---
+++ src/main/java/corruptthespire/events/corrupted/CorruptedShrine.java
@@ -0,0 +1,111 @@
+package corruptthespire.events.corrupted;
+
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.core.Settings;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.events.AbstractImageEvent;
+import com.megacrit.cardcrawl.helpers.RelicLibrary;
+import com.megacrit.cardcrawl.localization.EventStrings;
+import com.megacrit.cardcrawl.relics.AbstractRelic;
+import com.megacrit.cardcrawl.rewards.RewardItem;
+import com.megacrit.cardcrawl.rooms.AbstractRoom;
+import com.megacrit.cardcrawl.vfx.cardManip.ShowCardAndObtainEffect;
+import corruptthespire.Cor;
+import corruptthespire.CorruptTheSpire;
+import corruptthespire.cards.corrupted.CorruptedCardUtil;
+
+import java.text.MessageFormat;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class CorruptedShrine extends AbstractImageEvent {
+    public static final String ID = "CorruptTheSpire:CorruptedShrine";
+    private static final EventStrings eventStrings = CardCrawlGame.languagePack.getEventString(ID);
+    private static final String NAME = eventStrings.NAME;
+    private static final String[] DESCRIPTIONS = eventStrings.DESCRIPTIONS;
+    private static final String[] OPTIONS = eventStrings.OPTIONS;
+    private static final String IMG = CorruptTheSpire.eventImage(ID);
+
+    private static final int CARDS = 2;
+
+    private int screenNum = 0;
+
+    public CorruptedShrine() {
+        super(NAME, DESCRIPTIONS[0], IMG);
+        this.noCardsInRewards = true;
+
+        imageEventText.setDialogOption(MessageFormat.format(OPTIONS[0], CARDS));
+        imageEventText.setDialogOption(OPTIONS[1]);
+        imageEventText.setDialogOption(OPTIONS[2]);
+    }
+
+    @Override
+    public void onEnterRoom() {
+        CardCrawlGame.music.playTempBGM("SHRINE");
+    }
+
+    @Override
+    public void update() {
+        super.update();
+        if (!AbstractDungeon.isScreenUp && !AbstractDungeon.gridSelectScreen.selectedCards.isEmpty()) {
+            AbstractCard c = AbstractDungeon.gridSelectScreen.selectedCards.get(0).makeStatEquivalentCopy();
+            c.inBottleFlame = false;
+            c.inBottleLightning = false;
+            c.inBottleTornado = false;
+            AbstractDungeon.effectList.add(new ShowCardAndObtainEffect(c, (float)Settings.WIDTH / 2.0F, (float)Settings.HEIGHT / 2.0F));
+            AbstractDungeon.gridSelectScreen.selectedCards.clear();
+            logMetricObtainCards(ID, "Pray", Collections.singletonList(c.cardID));
+        }
+    }
+
+    @Override
+    protected void buttonEffect(int buttonPressed) {
+        switch (screenNum) {
+            case 0:
+                switch (buttonPressed) {
+                    case 0: // Beseech
+                        this.showCardReward(CARDS);
+                        logMetric(ID, "Beseech");
+
+                        this.imageEventText.updateBodyText(DESCRIPTIONS[1]);
+                        this.screenNum = 1;
+                        this.imageEventText.updateDialogOption(0, OPTIONS[2]);
+                        this.imageEventText.clearRemainingOptions();
+                        break;
+                    case 1: // Pray
+                        AbstractDungeon.gridSelectScreen.open(AbstractDungeon.player.masterDeck, 1, OPTIONS[3], false, false, false, false);
+
+                        this.imageEventText.updateBodyText(DESCRIPTIONS[2]);
+                        this.screenNum = 1;
+                        this.imageEventText.updateDialogOption(0, OPTIONS[2]);
+                        this.imageEventText.clearRemainingOptions();
+                        break;
+                    case 2: // Leave
+                        logMetricIgnored(ID);
+
+                        this.imageEventText.updateBodyText(DESCRIPTIONS[3]);
+                        this.screenNum = 1;
+                        this.imageEventText.updateDialogOption(0, OPTIONS[2]);
+                        this.imageEventText.clearRemainingOptions();
+                        break;
+                }
+                break;
+            default:
+                this.openMap();
+                break;
+        }
+    }
+
+    private void showCardReward(int numRewards) {
+        AbstractDungeon.getCurrRoom().rewards.clear();
+        for(int i = 0; i < numRewards; ++i) {
+            RewardItem reward = CorruptedCardUtil.getCorruptedCardReward();
+            AbstractDungeon.getCurrRoom().addCardReward(reward);
+        }
+
+        AbstractDungeon.getCurrRoom().phase = AbstractRoom.RoomPhase.COMPLETE;
+        AbstractDungeon.combatRewardScreen.open();
+    }
+}
