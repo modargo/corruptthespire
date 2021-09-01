26c8006131910ec363be4b6654e3ba06805de638
modargo <modargo@gmail.com>
2021-09-01 12:39:46 -0400

    Tweak the interpolation used for the corrupted icon animation

--- src/main/java/corruptthespire/patches/ShowCorruptNodesOnMapPatch.java
+++ src/main/java/corruptthespire/patches/ShowCorruptNodesOnMapPatch.java
@@ -6,6 +6,7 @@ import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Interpolation;
+import com.badlogic.gdx.math.MathUtils;
 import com.evacipated.cardcrawl.modthespire.lib.*;
 import com.megacrit.cardcrawl.core.Settings;
 import com.megacrit.cardcrawl.helpers.Hitbox;
@@ -48,9 +49,20 @@ public class ShowCorruptNodesOnMapPatch {
                     sb.draw(IMAGE, (float)__instance.x * spacingX + offsetX - 64.0F + __instance.offsetX + imgWidth * scale, (float)__instance.y * Settings.MAP_DST_Y + offsetY + DungeonMapScreen.offsetY - 64.0F + __instance.offsetY, 64.0F, 64.0F, 128.0F, 128.0F, scale * Settings.scale * 2.0F, scale * Settings.scale * 2.0F, 0.0F, 0, 0, 128, 128, false, false);
                 }
 
-                float tmp = Interpolation.exp10In.apply(0.0F, 4.0F, ___flameVfxTimer / (2.0F * GLOW_CYCLE));
+                //exp10In interpolation with input range from 0.0 to 0.5 results in output range from 0.0 to 0.12
+                //Future experiment can be to linearly map between those numbers instead
+                //The value of 0.12F for the maximum additional scale here was reached by empirically examining the
+                //interpolation that pulsing relics using, Interpolation.exp10In with an input range from 0.0 to 0.5,
+                //and observing that it gives an output range of 0.0 to 0.12. This was then tweaked based on how things
+                //looked with the various smoother interpolations (linear, sine).
+                float maxAdditionalScale = 0.09F;
+                float p = Math.abs((GLOW_CYCLE / 2.0F) - ___flameVfxTimer) / (GLOW_CYCLE / 2.0F);
+                //float tmp = Interpolation.exp10In.apply(0.0F, 4.0F, p / 2.0F);
+                //float tmp = p * maxAdditionalScale;
+                float tmp = (1.0F - MathUtils.sin(___flameVfxTimer / GLOW_CYCLE * MathUtils.PI)) * maxAdditionalScale;
+                //logger.info("tmp: " + tmp);
                 sb.setBlendFunction(770, 1);
-                float alpha = ___flameVfxTimer * ALPHA_RANGE;
+                float alpha = p * ALPHA_RANGE;
                 sb.setColor(new Color(1.0F, 1.0F, 1.0F, alpha));
                 if (!Settings.isMobile) {
                     sb.draw(IMAGE, (float)__instance.x * spacingX + offsetX - 64.0F + __instance.offsetX + imgWidth * scale, (float)__instance.y * Settings.MAP_DST_Y + offsetY + DungeonMapScreen.offsetY - 64.0F + __instance.offsetY, 64.0F, 64.0F, 128.0F, 128.0F, scale * Settings.scale + tmp, scale * Settings.scale + tmp, 0.0F, 0, 0, 128, 128, false, false);
