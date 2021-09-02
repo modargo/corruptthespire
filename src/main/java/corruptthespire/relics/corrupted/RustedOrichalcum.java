f8c017ef23354d54f1adf09cac7e2dcda2ba1dfe
modargo <modargo@gmail.com>
2021-09-02 07:44:22 -0400

    Change Rusted Orichalcum to give 13 block, for flavor

--- src/main/java/corruptthespire/relics/corrupted/RustedOrichalcum.java
+++ src/main/java/corruptthespire/relics/corrupted/RustedOrichalcum.java
@@ -17,7 +17,7 @@ public class RustedOrichalcum extends AbstractCorruptedRelic {
     private static final Texture IMG = TextureLoader.getTexture(CorruptTheSpire.relicImage(ID));
     private static final Texture OUTLINE = TextureLoader.getTexture(CorruptTheSpire.relicOutlineImage(ID));
     private static final int TURNS = 3;
-    private static final int BLOCK = 12;
+    private static final int BLOCK = 13;
     private static final int CONSTRICTED = 6;
 
     public RustedOrichalcum() {
