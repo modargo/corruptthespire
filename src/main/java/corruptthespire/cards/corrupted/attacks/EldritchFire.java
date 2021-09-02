7383491bec3e2646823b75922f1bd95c3d0d54bb
modargo <modargo@gmail.com>
2021-09-02 13:52:11 -0400

    Fix Eldritch Fire cost

--- src/main/java/corruptthespire/cards/corrupted/attacks/EldritchFire.java
+++ src/main/java/corruptthespire/cards/corrupted/attacks/EldritchFire.java
@@ -14,7 +14,7 @@ public class EldritchFire extends AbstractCorruptedCard {
     private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
     public static final String NAME = cardStrings.NAME;
     public static final String DESCRIPTION = cardStrings.DESCRIPTION;
-    private static final int COST = -2;
+    private static final int COST = -1;
     private static final int DAMAGE = 8;
     private static final int UPGRADE_DAMAGE = 3;
     private static final int STRENGTH = 2;
