3ae49c5c828624174918e84036efd974fdf9db24
modargo <modargo@gmail.com>
2021-09-02 16:51:15 -0400

    Increase abysstouched per turn for Black Omen

--- src/main/java/corruptthespire/cards/corrupted/powers/BlackOmen.java
+++ src/main/java/corruptthespire/cards/corrupted/powers/BlackOmen.java
@@ -16,7 +16,7 @@ public class BlackOmen extends AbstractCorruptedCard {
     public static final String NAME = cardStrings.NAME;
     public static final String DESCRIPTION = cardStrings.DESCRIPTION;
     private static final int COST = 1;
-    private static final int ABYSSTOUCHED = 2;
+    private static final int ABYSSTOUCHED = 3;
     private static final int UPGRADE_ABYSSTOUCHED = 1;
 
     public BlackOmen() {
