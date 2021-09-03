4d61803e457d2bdc77af212626d3007505b833d2
modargo <modargo@gmail.com>
2021-09-02 21:35:54 -0400

    Add logs for debugging campfire behavior

---
+++ src/main/java/corruptthespire/patches/campfire/CampfireDebuggingPatch.java
@@ -0,0 +1,25 @@
+package corruptthespire.patches.campfire;
+
+import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
+import com.evacipated.cardcrawl.modthespire.lib.SpirePostfixPatch;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.vfx.campfire.CampfireSmithEffect;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class CampfireDebuggingPatch {
+    private static final Logger logger = LogManager.getLogger(CampfireDebuggingPatch.class.getName());
+
+    @SpirePatch(clz = CampfireSmithEffect.class, method = "update")
+    public static class CampfireSmithEffectPatch {
+        @SpirePostfixPatch
+        public static void campfireSmithEffectUpdateLog(CampfireSmithEffect __instance) {
+            logger.info("AbstractDungeon.isScreenUp: " + AbstractDungeon.isScreenUp);
+            logger.info("__instance.duration: " + __instance.duration);
+            logger.info("AbstractDungeon.effectsQueue.size(): " + AbstractDungeon.effectsQueue.size());
+            logger.info("AbstractDungeon.getCurrRoom().phase: " + AbstractDungeon.getCurrRoom().phase);
+
+        }
+
+    }
+}
