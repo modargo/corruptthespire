9d848215ab2861d6df931b74c67a6b35c2aa6c71
modargo <modargo@gmail.com>
2021-09-03 20:05:29 -0400

    Remove unintentional corruption from Death Touch

--- src/main/java/corruptthespire/cards/corrupted/attacks/DeathTouch.java
+++ src/main/java/corruptthespire/cards/corrupted/attacks/DeathTouch.java
@@ -25,10 +25,9 @@ public class DeathTouch extends AbstractCorruptedCard {
     private static final int DAMAGE = 1;
     private static final int ABYSSTOUCHED = 10;
     private static final int UPGRADE_ABYSSTOUCHED = 2;
-    private static final int CORRUPTION = 1;
 
     public DeathTouch() {
-        super(ID, NAME, IMG, COST, MessageFormat.format(DESCRIPTION, CORRUPTION), CardType.ATTACK, CardTarget.ENEMY);
+        super(ID, NAME, IMG, COST, DESCRIPTION, CardType.ATTACK, CardTarget.ENEMY);
         this.baseDamage = DAMAGE;
         this.magicNumber = this.baseMagicNumber = ABYSSTOUCHED;
     }
@@ -45,6 +44,5 @@ public class DeathTouch extends AbstractCorruptedCard {
     public void use(AbstractPlayer p, AbstractMonster m) {
         this.addToBot(new DamageAction(m, new DamageInfo(p, this.damage, this.damageTypeForTurn), AbstractGameAction.AttackEffect.SLASH_DIAGONAL));
         this.addToBot(new ApplyPowerAction(m, p, PowerUtil.abysstouched(m, this.magicNumber), this.magicNumber));
-        this.addToBot(new GainCorruptionAction(CORRUPTION));
     }
 }
