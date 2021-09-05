564f6d66b21bfe9991b716b585154f2421c24e46
modargo <modargo@gmail.com>
2021-09-04 21:51:35 -0400

    Buff Corrupted Form

--- src/main/java/corruptthespire/cards/corrupted/powers/CorruptedForm.java
+++ src/main/java/corruptthespire/cards/corrupted/powers/CorruptedForm.java
@@ -19,8 +19,8 @@ public class CorruptedForm extends AbstractCorruptedCard {
     public static final String NAME = cardStrings.NAME;
     public static final String DESCRIPTION = cardStrings.DESCRIPTION;
     private static final int COST = 3;
-    private static final int CORRUPTION = 6;
-    private static final int PERCENT = 40;
+    private static final int CORRUPTION = 5;
+    private static final int PERCENT = 50;
 
     public CorruptedForm() {
         super(ID, NAME, IMG, COST, MessageFormat.format(DESCRIPTION, CORRUPTION), CardType.POWER, CardTarget.SELF);
