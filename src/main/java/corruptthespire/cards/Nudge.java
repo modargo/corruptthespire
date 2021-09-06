7a78e5ef40032a2ec3fa7bc710d06ae0044d51e0
modargo <modargo@gmail.com>
2021-09-06 11:28:03 -0400

    Try out Nudge upgrade also increasing Scry by 1

--- src/main/java/corruptthespire/cards/Nudge.java
+++ src/main/java/corruptthespire/cards/Nudge.java
@@ -30,9 +30,9 @@ public class Nudge extends CustomCard {
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
         this.addToBot(new ApplyPowerAction(p, p, new DexterityPower(p, 1), 1));
-        this.addToBot(new ScryAction(3));
+        this.addToBot(new ScryAction(this.upgraded ? 4 : 3));
         this.addToBot(new DrawCardAction(1));
-        this.addToBot(new ApplyPowerAction(p, p, new DrawCardNextTurnPower(p, this.upgraded ? 2 : 1), this.upgraded ? 2 : 1));
+        this.addToBot(new ApplyPowerAction(p, p, new DrawCardNextTurnPower(p, this.upgraded ? 2 : 1)));
         this.addToBot(new ArmamentsAction(false));
     }
 
