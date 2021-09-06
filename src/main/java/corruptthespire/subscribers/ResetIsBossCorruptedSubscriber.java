a3183e52af0c6af19ae83f2ecd5b20adfa8c0f55
modargo <modargo@gmail.com>
2021-09-06 12:54:07 -0400

    Clean up logs and references

--- src/main/java/corruptthespire/subscribers/ResetIsBossCorruptedSubscriber.java
+++ src/main/java/corruptthespire/subscribers/ResetIsBossCorruptedSubscriber.java
@@ -2,14 +2,10 @@ package corruptthespire.subscribers;
 
 import basemod.interfaces.StartActSubscriber;
 import corruptthespire.map.CorruptMap;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 public class ResetIsBossCorruptedSubscriber implements StartActSubscriber {
-    private static final Logger logger = LogManager.getLogger(ResetIsBossCorruptedSubscriber.class.getName());
     @Override
     public void receiveStartAct() {
-        logger.info("Starting new act, resetting boss corrupted marker");
         CorruptMap.setIsBossCorrupted(false);
     }
 }
--- src/main/java/corruptthespire/subscribers/ResetNormalMonsterCountSubscriber.java
+++ src/main/java/corruptthespire/subscribers/ResetNormalMonsterCountSubscriber.java
@@ -2,9 +2,6 @@ package corruptthespire.subscribers;
 
 import basemod.interfaces.StartActSubscriber;
 import corruptthespire.Cor;
-import corruptthespire.map.CorruptMap;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 public class ResetNormalMonsterCountSubscriber implements StartActSubscriber {
     @Override
