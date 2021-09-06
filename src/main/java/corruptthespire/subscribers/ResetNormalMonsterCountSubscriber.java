02cd9dd114e4b59d571894e8648a419f83104103
modargo <modargo@gmail.com>
2021-09-06 12:53:25 -0400

    Remove logs that aren't needed

--- src/main/java/corruptthespire/subscribers/ResetNormalMonsterCountSubscriber.java
+++ src/main/java/corruptthespire/subscribers/ResetNormalMonsterCountSubscriber.java
@@ -7,10 +7,8 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class ResetNormalMonsterCountSubscriber implements StartActSubscriber {
-    private static final Logger logger = LogManager.getLogger(ResetNormalMonsterCountSubscriber.class.getName());
     @Override
     public void receiveStartAct() {
-        logger.info("Starting new act, resetting normal monster count");
         Cor.flags.normalMonsterCount = 0;
     }
 }
