0adca8411082f70be5d44da9f95adf6e3523d8bb
modargo <modargo@gmail.com>
2021-09-15 15:49:34 -0400

    Add an icon for random upgrade rewards

--- src/main/java/corruptthespire/rewards/RandomUpgradeReward.java
+++ src/main/java/corruptthespire/rewards/RandomUpgradeReward.java
@@ -7,8 +7,7 @@ import corruptthespire.cards.CardUtil;
 
 public class RandomUpgradeReward extends AbstractCorruptTheSpireReward {
     public static final String[] TEXT = CardCrawlGame.languagePack.getUIString("CorruptTheSpire:Rewards").TEXT;
-    //TODO Better icon
-    private static final Texture ICON = ImageMaster.TP_HP;
+    private static final Texture ICON = ImageMaster.loadImage("images/ui/run_mods/shiny.png");
 
     public RandomUpgradeReward() {
         super(ICON, TEXT[1], CustomRewardTypes.CORRUPTTHESPIRE_RANDOMUPGRADE);
