28f5ce1db96698fcfc413f067946ed0f9f1b04fd
modargo <modargo@gmail.com>
2021-11-14 14:00:40 -0500

    Reduce Wicked Ward block slightly

--- src/main/java/corruptthespire/cards/corrupted/skills/WickedWard.java
+++ src/main/java/corruptthespire/cards/corrupted/skills/WickedWard.java
@@ -18,10 +18,10 @@ public class WickedWard extends AbstractCorruptedCard {
     public static final String NAME = cardStrings.NAME;
     public static final String DESCRIPTION = cardStrings.DESCRIPTION;
     private static final int COST = 0;
-    private static final int BLOCK = 6;
+    private static final int BLOCK = 5;
     private static final int UPGRADE_BLOCK = 2;
     private static final int ADDITIONAL_BLOCK = 4;
-    private static final int UPGRADE_ADDITIONAL_BLOCK = 2;
+    private static final int UPGRADE_ADDITIONAL_BLOCK = 1;
     private static final int CORRUPTION_THRESHOLD = 50;
 
     public WickedWard() {
