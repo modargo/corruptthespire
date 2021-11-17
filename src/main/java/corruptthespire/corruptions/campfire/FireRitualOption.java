9bd783d2c262acfe22bc6c4523ced88e056a32d7
modargo <modargo@gmail.com>
2021-11-16 22:52:54 -0500

    Reduce Fire Ritual corruption reduction by 1

--- src/main/java/corruptthespire/corruptions/campfire/FireRitualOption.java
+++ src/main/java/corruptthespire/corruptions/campfire/FireRitualOption.java
@@ -15,7 +15,7 @@ public class FireRitualOption extends AbstractCampfireOption {
     private static final UIStrings uiStrings = CardCrawlGame.languagePack.getUIString("CorruptTheSpire:FireRitualOption");
     public static final String[] TEXT = uiStrings.TEXT;
     private static final Texture IMG = ImageMaster.loadImage("images/ui/campfire/meditate.png");
-    private static final int CORRUPTION_PER_FRAGMENT = 6;
+    private static final int CORRUPTION_PER_FRAGMENT = 5;
     private static final int MAX_HEALTH_PER_FRAGMENT = 1;
     private final int fragmentCost;
     private final int corruptionReduction;
