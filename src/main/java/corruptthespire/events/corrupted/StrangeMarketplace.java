735b8431655d0f1a36c393bb0bf5f7667bcc1df9
modargo <modargo@gmail.com>
2022-01-09 18:45:41 -0500

    Change Strange Marketplace to have an option to get corrupted potions

--- src/main/java/corruptthespire/events/corrupted/StrangeMarketplace.java
+++ src/main/java/corruptthespire/events/corrupted/StrangeMarketplace.java
@@ -4,11 +4,15 @@ import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.core.Settings;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.events.AbstractImageEvent;
+import com.megacrit.cardcrawl.events.GenericEventDialog;
 import com.megacrit.cardcrawl.helpers.RelicLibrary;
 import com.megacrit.cardcrawl.localization.EventStrings;
 import com.megacrit.cardcrawl.relics.AbstractRelic;
+import com.megacrit.cardcrawl.rewards.RewardItem;
+import com.megacrit.cardcrawl.rooms.AbstractRoom;
 import corruptthespire.Cor;
 import corruptthespire.CorruptTheSpire;
+import corruptthespire.potions.PotionUtil;
 
 import java.text.MessageFormat;
 
@@ -22,20 +26,25 @@ public class StrangeMarketplace extends AbstractImageEvent {
 
     public static final int GOLD = 40;
     public static final int A15_GOLD = 60;
+    public static final int POTIONS = 3;
+    public static final int A15_POTIONS = 2;
 
     private final AbstractRelic relic;
     private final int gold;
+    private final int potions;
 
     private int screenNum = 0;
 
     public StrangeMarketplace() {
         super(NAME, DESCRIPTIONS[0], IMG);
+        this.noCardsInRewards = true;
 
         this.relic = RelicLibrary.getRelic(Cor.returnRandomCorruptedRelicKey());
         this.gold = AbstractDungeon.ascensionLevel >= 15 ? A15_GOLD : GOLD;
+        this.potions = AbstractDungeon.ascensionLevel >= 15 ? A15_POTIONS : POTIONS;
 
         imageEventText.setDialogOption(MessageFormat.format(OPTIONS[0], this.relic.name, this.gold), this.relic);
-        imageEventText.setDialogOption(OPTIONS[1]);
+        imageEventText.setDialogOption(MessageFormat.format(OPTIONS[1], this.potions));
     }
 
     @Override
@@ -50,16 +59,21 @@ public class StrangeMarketplace extends AbstractImageEvent {
 
                         this.imageEventText.updateBodyText(DESCRIPTIONS[1]);
                         this.screenNum = 1;
-                        this.imageEventText.updateDialogOption(0, OPTIONS[1]);
+                        this.imageEventText.updateDialogOption(0, OPTIONS[2]);
                         this.imageEventText.clearRemainingOptions();
                         break;
-                    case 1: // Leave
-                        logMetricIgnored(ID);
+                    case 1: // Sample
+                        logMetric(ID, "Sample");
 
-                        this.imageEventText.updateBodyText(DESCRIPTIONS[2]);
+                        AbstractDungeon.getCurrRoom().rewards.clear();
+                        for (int i = 0; i < this.potions; i++) {
+                            AbstractDungeon.getCurrRoom().rewards.add(new RewardItem(PotionUtil.getRandomCorruptedPotion(AbstractDungeon.miscRng)));
+                        }
+
+                        GenericEventDialog.hide();
+                        AbstractDungeon.getCurrRoom().phase = AbstractRoom.RoomPhase.COMPLETE;
+                        AbstractDungeon.combatRewardScreen.open();
                         this.screenNum = 1;
-                        this.imageEventText.updateDialogOption(0, OPTIONS[1]);
-                        this.imageEventText.clearRemainingOptions();
                         break;
                 }
                 break;
