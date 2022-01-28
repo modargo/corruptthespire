5b842ef784e63847978afa8170f06ef08092add0
modargo <modargo@gmail.com>
2022-01-28 16:42:47 -0500

    Reduce Divine Strike damage slightly

--- src/main/java/corruptthespire/cards/corrupted/attacks/DivineStrike.java
+++ src/main/java/corruptthespire/cards/corrupted/attacks/DivineStrike.java
@@ -19,7 +19,7 @@ public class DivineStrike extends AbstractCorruptedCard {
     public static final String NAME = cardStrings.NAME;
     public static final String DESCRIPTION = cardStrings.DESCRIPTION;
     private static final int COST = 1;
-    private static final int DAMAGE = 9;
+    private static final int DAMAGE = 7;
     private static final int UPGRADE_DAMAGE = 2;
     private static final int MANTRA = 1;
     private static final int UPGRADE_MANTRA = 1;
