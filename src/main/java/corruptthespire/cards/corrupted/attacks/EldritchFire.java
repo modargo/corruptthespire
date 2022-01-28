21d8d506079c885b76ef0e05d37906487d51eea9
modargo <modargo@gmail.com>
2022-01-28 16:41:25 -0500

    Reduce Eldritch Fire damage slightly

--- src/main/java/corruptthespire/cards/corrupted/attacks/EldritchFire.java
+++ src/main/java/corruptthespire/cards/corrupted/attacks/EldritchFire.java
@@ -15,7 +15,7 @@ public class EldritchFire extends AbstractCorruptedCard {
     public static final String NAME = cardStrings.NAME;
     public static final String DESCRIPTION = cardStrings.DESCRIPTION;
     private static final int COST = -1;
-    private static final int DAMAGE = 13;
+    private static final int DAMAGE = 11;
     private static final int UPGRADE_DAMAGE = 3;
     private static final int STRENGTH = 2;
     private static final int UPGRADE_STRENGTH = 1;
