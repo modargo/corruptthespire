e89a77ddbd8dfea2c6ff37351545cc12664b4291
modargo <modargo@gmail.com>
2022-01-28 23:58:06 -0500

    Further reduce Divine Strike damage and remove upgrade damage

--- src/main/java/corruptthespire/cards/corrupted/attacks/DivineStrike.java
+++ src/main/java/corruptthespire/cards/corrupted/attacks/DivineStrike.java
@@ -19,8 +19,7 @@ public class DivineStrike extends AbstractCorruptedCard {
     public static final String NAME = cardStrings.NAME;
     public static final String DESCRIPTION = cardStrings.DESCRIPTION;
     private static final int COST = 1;
-    private static final int DAMAGE = 7;
-    private static final int UPGRADE_DAMAGE = 2;
+    private static final int DAMAGE = 6;
     private static final int MANTRA = 1;
     private static final int UPGRADE_MANTRA = 1;
 
@@ -35,7 +34,6 @@ public class DivineStrike extends AbstractCorruptedCard {
     @Override
     public void upgrade() {
         if (!this.upgraded) {
-            this.upgradeDamage(UPGRADE_DAMAGE);
             this.upgradeMagicNumber(UPGRADE_MANTRA);
             this.upgradeName();
         }
