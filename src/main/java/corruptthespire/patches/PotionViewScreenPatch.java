b84f93a49b38e1a906f2550041aef04f80784cc3
modargo <modargo@gmail.com>
2022-01-29 20:06:58 -0500

    Add corrupted potions to the potion compendium

---
+++ src/main/java/corruptthespire/patches/PotionViewScreenPatch.java
@@ -0,0 +1,44 @@
+package corruptthespire.patches;
+
+import basemod.ReflectionHacks;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.evacipated.cardcrawl.modthespire.lib.*;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.potions.AbstractPotion;
+import com.megacrit.cardcrawl.screens.compendium.PotionViewScreen;
+import com.megacrit.cardcrawl.screens.mainMenu.MenuCancelButton;
+import corruptthespire.potions.PotionUtil;
+import javassist.CtBehavior;
+
+import java.util.ArrayList;
+
+public class PotionViewScreenPatch {
+    private static final String[] TEXT = CardCrawlGame.languagePack.getUIString("CorruptTheSpire:PotionViewScreen").TEXT;
+    private static final ArrayList<AbstractPotion> allCorruptedPotions = PotionUtil.getAllCorruptedPotions();
+
+    @SpirePatch(clz = PotionViewScreen.class, method = "render")
+    public static class PotionViewScreenRenderPatch {
+        @SpireInsertPatch(locator = Locator.class, localvars = { "sb" })
+        public static void renderCorruptedPotionsList(PotionViewScreen __instance, SpriteBatch sb) {
+            ReflectionHacks.RMethod m = ReflectionHacks.privateMethod(PotionViewScreen.class, "renderList", SpriteBatch.class, String.class, String.class, ArrayList.class);
+            m.invoke(__instance, sb, TEXT[0], TEXT[1], allCorruptedPotions);
+        }
+
+        private static class Locator extends SpireInsertLocator {
+            @Override
+            public int[] Locate(CtBehavior ctMethodToPatch) throws Exception {
+                Matcher finalMatcher = new Matcher.MethodCallMatcher(MenuCancelButton.class, "render");
+                return LineFinder.findInOrder(ctMethodToPatch, finalMatcher);
+            }
+        }
+    }
+
+    @SpirePatch(clz = PotionViewScreen.class, method = "update")
+    public static class PotionViewScreenUpdatePatch {
+        @SpirePostfixPatch
+        public static void updateCorruptedPotionsList(PotionViewScreen __instance) {
+            ReflectionHacks.RMethod m = ReflectionHacks.privateMethod(PotionViewScreen.class, "updateList", ArrayList.class);
+            m.invoke(__instance, allCorruptedPotions);
+        }
+    }
+}
--- src/main/java/corruptthespire/potions/PotionUtil.java
+++ src/main/java/corruptthespire/potions/PotionUtil.java
@@ -4,11 +4,17 @@ import com.megacrit.cardcrawl.helpers.PotionHelper;
 import com.megacrit.cardcrawl.potions.AbstractPotion;
 import com.megacrit.cardcrawl.random.Random;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class PotionUtil {
     public static final List<String> corruptedPotionIds = Arrays.asList(AbyssalBrew.POTION_ID, CorruptedPotion.POTION_ID, VoidBomb.POTION_ID, DarkElixir.POTION_ID, ProfaneMixture.POTION_ID);
+
+    public static ArrayList<AbstractPotion> getAllCorruptedPotions() {
+        return corruptedPotionIds.stream().map(PotionHelper::getPotion).collect(Collectors.toCollection(ArrayList::new));
+    }
 
     public static AbstractPotion getRandomCorruptedPotion(Random rng) {
         String potionId = corruptedPotionIds.get(rng.random(corruptedPotionIds.size() - 1));
