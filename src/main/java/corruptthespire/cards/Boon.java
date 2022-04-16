dec466e746bf389c2381cab7a6210935deff4adc
modargo <modargo@gmail.com>
2022-04-15 20:50:32 -0400

    Reduce Boon block and upgrade block by 1

--- src/main/java/corruptthespire/cards/Boon.java
+++ src/main/java/corruptthespire/cards/Boon.java
@@ -23,8 +23,8 @@ public class Boon extends CustomCard {
     public static final String DESCRIPTION = cardStrings.DESCRIPTION;
     public static final String UPGRADE_DESCRIPTION = cardStrings.UPGRADE_DESCRIPTION;
     private static final int COST = 0;
-    private static final int BLOCK = 2;
-    private static final int UPGRADE_BLOCK = 2;
+    private static final int BLOCK = 1;
+    private static final int UPGRADE_BLOCK = 1;
     private static final int DRAW = 1;
     private static final int UPGRADE_DRAW = 1;
 
