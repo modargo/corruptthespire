86def8484fed6b81123c265e1361553af4dd6283
modargo <modargo@gmail.com>
2022-06-07 13:49:23 -0400

    Remove unused variable

--- src/main/java/corruptthespire/cards/corrupted/powers/ElementalBarrier.java
+++ src/main/java/corruptthespire/cards/corrupted/powers/ElementalBarrier.java
@@ -15,7 +15,6 @@ public class ElementalBarrier extends AbstractCorruptedCard {
     private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
     public static final String NAME = cardStrings.NAME;
     public static final String DESCRIPTION = cardStrings.DESCRIPTION;
-    public static final String UPGRADE_DESCRIPTION = cardStrings.UPGRADE_DESCRIPTION;
     private static final int COST = 1;
     private static final int BLOCK = 1;
     private static final int UPGRADE_BLOCK = 1;
