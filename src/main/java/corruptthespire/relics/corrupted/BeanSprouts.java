43d88aade9f5e408e967cb1e1ea35c67830590bb
modargo <modargo@gmail.com>
2022-07-18 12:41:48 -0400

    Add relic stats for Bean Sprouts

--- src/main/java/corruptthespire/relics/corrupted/BeanSprouts.java
+++ src/main/java/corruptthespire/relics/corrupted/BeanSprouts.java
@@ -1,6 +1,9 @@
 package corruptthespire.relics.corrupted;
 
 import com.badlogic.gdx.graphics.Texture;
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.actions.common.GainBlockAction;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
@@ -10,7 +13,12 @@ import com.megacrit.cardcrawl.relics.AbstractRelic;
 import corruptthespire.CorruptTheSpire;
 import corruptthespire.util.TextureLoader;
 
+import java.text.DecimalFormat;
 import java.text.MessageFormat;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class BeanSprouts extends AbstractCorruptedRelic {
     public static final String ID = "CorruptTheSpire:BeanSprouts";
@@ -18,6 +26,10 @@ public class BeanSprouts extends AbstractCorruptedRelic {
     private static final Texture OUTLINE = TextureLoader.getTexture(CorruptTheSpire.relicOutlineImage(ID));
     public static final int STRENGTH = 1;
     public static final int BLOCK = 3;
+
+    private static final Map<String, Integer> stats = new HashMap<>();
+    private static final String STRENGTH_STAT = "strength";
+    private static final String BLOCK_STAT = "block";
 
     public BeanSprouts() {
         super(ID, IMG, OUTLINE, RelicTier.SPECIAL, LandingSound.SOLID);
@@ -29,6 +41,7 @@ public class BeanSprouts extends AbstractCorruptedRelic {
     }
 
     public void trigger() {
+        incrementStrengthAndBlockStat();
         this.flash();
         if (AbstractDungeon.actionManager.turnHasEnded) {
             this.addToTop(new ApplyPowerAction(AbstractDungeon.player, AbstractDungeon.player, new NextTurnBlockPower(AbstractDungeon.player, BLOCK)));
@@ -42,5 +55,48 @@ public class BeanSprouts extends AbstractCorruptedRelic {
     @Override
     public AbstractRelic makeCopy() {
         return new BeanSprouts();
+    }
+
+    public String getStatsDescription() {
+        return MessageFormat.format(DESCRIPTIONS[1], stats.get(STRENGTH_STAT), stats.get(BLOCK_STAT));
+    }
+
+    public String getExtendedStatsDescription(int totalCombats, int totalTurns) {
+        DecimalFormat format = new DecimalFormat("#.###");
+        float strength = stats.get(STRENGTH_STAT);
+        float block = stats.get(BLOCK_STAT);
+        String strengthPerTurn = format.format(strength / Math.max(totalTurns, 1));
+        String blockPerTurn = format.format(block / Math.max(totalTurns, 1));
+        String strengthPerCombat = format.format(strength / Math.max(totalCombats, 1));
+        String blockPerCombat = format.format(block / Math.max(totalCombats, 1));
+        return getStatsDescription() + MessageFormat.format(DESCRIPTIONS[2], strengthPerTurn, blockPerTurn, strengthPerCombat, blockPerCombat);
+    }
+
+    public void resetStats() {
+        stats.put(STRENGTH_STAT, 0);
+        stats.put(BLOCK_STAT, 0);
+    }
+
+    public JsonElement onSaveStats() {
+        Gson gson = new Gson();
+        List<Integer> statsToSave = new ArrayList<>();
+        statsToSave.add(stats.get(STRENGTH_STAT));
+        statsToSave.add(stats.get(BLOCK_STAT));
+        return gson.toJsonTree(statsToSave);
+    }
+
+    public void onLoadStats(JsonElement jsonElement) {
+        if (jsonElement != null) {
+            JsonArray jsonArray = jsonElement.getAsJsonArray();
+            stats.put(STRENGTH_STAT, jsonArray.get(0).getAsInt());
+            stats.put(BLOCK_STAT, jsonArray.get(1).getAsInt());
+        } else {
+            resetStats();
+        }
+    }
+
+    public static void incrementStrengthAndBlockStat() {
+        stats.put(STRENGTH_STAT, stats.getOrDefault(STRENGTH_STAT, 0) + STRENGTH);
+        stats.put(BLOCK_STAT, stats.getOrDefault(BLOCK_STAT, 0) + BLOCK);
     }
 }
