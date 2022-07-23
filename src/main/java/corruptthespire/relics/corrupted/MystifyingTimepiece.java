46a9c391ece3fe57a4bb3e8504d6824515a10036
modargo <modargo@gmail.com>
2022-07-23 18:19:53 -0400

    Add relic stats for Mystifying Timepiece

--- src/main/java/corruptthespire/relics/corrupted/MystifyingTimepiece.java
+++ src/main/java/corruptthespire/relics/corrupted/MystifyingTimepiece.java
@@ -1,6 +1,9 @@
 package corruptthespire.relics.corrupted;
 
 import com.badlogic.gdx.graphics.Texture;
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
 import com.megacrit.cardcrawl.actions.common.DrawCardAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
@@ -9,7 +12,12 @@ import com.megacrit.cardcrawl.relics.AbstractRelic;
 import corruptthespire.CorruptTheSpire;
 import corruptthespire.util.TextureLoader;
 
+import java.text.DecimalFormat;
 import java.text.MessageFormat;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class MystifyingTimepiece extends AbstractCorruptedRelic {
     public static final String ID = "CorruptTheSpire:MystifyingTimepiece";
@@ -17,6 +25,9 @@ public class MystifyingTimepiece extends AbstractCorruptedRelic {
     private static final Texture OUTLINE = TextureLoader.getTexture(CorruptTheSpire.relicOutlineImage(ID));
     private static final int COST = 0;
     private static final int CARDS = 1;
+
+    private static final Map<String, Integer> stats = new HashMap<>();
+    private static final String DRAW_STAT = "draw";
 
     private boolean firstTurn = true;
 
@@ -33,6 +44,7 @@ public class MystifyingTimepiece extends AbstractCorruptedRelic {
     @Override
     public void atTurnStartPostDraw() {
         if (this.counter == 0 && !this.firstTurn) {
+            incrementDrawStat();
             this.addToBot(new DrawCardAction(AbstractDungeon.player, CARDS));
         } else {
             this.firstTurn = false;
@@ -65,5 +77,41 @@ public class MystifyingTimepiece extends AbstractCorruptedRelic {
     @Override
     public AbstractRelic makeCopy() {
         return new MystifyingTimepiece();
+    }
+
+    public String getStatsDescription() {
+        return MessageFormat.format(DESCRIPTIONS[1], stats.get(DRAW_STAT));
+    }
+
+    public String getExtendedStatsDescription(int totalCombats, int totalTurns) {
+        DecimalFormat format = new DecimalFormat("#.###");
+        float draw = stats.get(DRAW_STAT);
+        String drawPerTurn = format.format(draw / Math.max(totalTurns, 1));
+        String drawPerCombat = format.format(draw / Math.max(totalCombats, 1));
+        return getStatsDescription() + MessageFormat.format(DESCRIPTIONS[2], drawPerTurn, drawPerCombat);
+    }
+
+    public void resetStats() {
+        stats.put(DRAW_STAT, 0);
+    }
+
+    public JsonElement onSaveStats() {
+        Gson gson = new Gson();
+        List<Integer> statsToSave = new ArrayList<>();
+        statsToSave.add(stats.get(DRAW_STAT));
+        return gson.toJsonTree(statsToSave);
+    }
+
+    public void onLoadStats(JsonElement jsonElement) {
+        if (jsonElement != null) {
+            JsonArray jsonArray = jsonElement.getAsJsonArray();
+            stats.put(DRAW_STAT, jsonArray.get(0).getAsInt());
+        } else {
+            resetStats();
+        }
+    }
+
+    public static void incrementDrawStat() {
+        stats.put(DRAW_STAT, stats.getOrDefault(DRAW_STAT, 0) + CARDS);
     }
 }
