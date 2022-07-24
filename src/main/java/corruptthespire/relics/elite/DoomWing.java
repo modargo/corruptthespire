ebaa65c0ca442910ca5fe994abbd8f5289dd158f
modargo <modargo@gmail.com>
2022-07-23 22:24:37 -0400

    Add relic stats for Doom Wing

--- src/main/java/corruptthespire/relics/elite/DoomWing.java
+++ src/main/java/corruptthespire/relics/elite/DoomWing.java
@@ -2,6 +2,9 @@ package corruptthespire.relics.elite;
 
 import basemod.abstracts.CustomRelic;
 import com.badlogic.gdx.graphics.Texture;
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.actions.common.RelicAboveCreatureAction;
 import com.megacrit.cardcrawl.actions.utility.UseCardAction;
@@ -12,14 +15,21 @@ import com.megacrit.cardcrawl.relics.AbstractRelic;
 import corruptthespire.CorruptTheSpire;
 import corruptthespire.util.TextureLoader;
 
+import java.text.DecimalFormat;
 import java.text.MessageFormat;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class DoomWing extends CustomRelic {
     public static final String ID = "CorruptTheSpire:DoomWing";
     private static final Texture IMG = TextureLoader.getTexture(CorruptTheSpire.relicImage(ID));
     private static final Texture OUTLINE = TextureLoader.getTexture(CorruptTheSpire.relicOutlineImage(ID));
-
     private static final int THORNS = 2;
+
+    private static final Map<String, Integer> stats = new HashMap<>();
+    private static final String THORNS_STAT = "thorns";
 
     public DoomWing() {
         super(ID, IMG, OUTLINE, RelicTier.SPECIAL, LandingSound.MAGICAL);
@@ -33,6 +43,7 @@ public class DoomWing extends CustomRelic {
     @Override
     public void onUseCard(AbstractCard card, UseCardAction action) {
         if (card.type == AbstractCard.CardType.POWER) {
+            incrementThornsStat();
             this.flash();
             this.addToTop(new ApplyPowerAction(AbstractDungeon.player, AbstractDungeon.player, new ThornsPower(AbstractDungeon.player, THORNS)));
             this.addToTop(new RelicAboveCreatureAction(AbstractDungeon.player, this));
@@ -53,5 +64,41 @@ public class DoomWing extends CustomRelic {
     @Override
     public AbstractRelic makeCopy() {
         return new DoomWing();
+    }
+
+    public String getStatsDescription() {
+        return MessageFormat.format(DESCRIPTIONS[1], stats.get(THORNS_STAT));
+    }
+
+    public String getExtendedStatsDescription(int totalCombats, int totalTurns) {
+        DecimalFormat format = new DecimalFormat("#.###");
+        float thorns = stats.get(THORNS_STAT);
+        String thornsPerTurn = format.format(thorns / Math.max(totalTurns, 1));
+        String thornsPerCombat = format.format(thorns / Math.max(totalCombats, 1));
+        return getStatsDescription() + MessageFormat.format(DESCRIPTIONS[2], thornsPerTurn, thornsPerCombat);
+    }
+
+    public void resetStats() {
+        stats.put(THORNS_STAT, 0);
+    }
+
+    public JsonElement onSaveStats() {
+        Gson gson = new Gson();
+        List<Integer> statsToSave = new ArrayList<>();
+        statsToSave.add(stats.get(THORNS_STAT));
+        return gson.toJsonTree(statsToSave);
+    }
+
+    public void onLoadStats(JsonElement jsonElement) {
+        if (jsonElement != null) {
+            JsonArray jsonArray = jsonElement.getAsJsonArray();
+            stats.put(THORNS_STAT, jsonArray.get(0).getAsInt());
+        } else {
+            resetStats();
+        }
+    }
+
+    public static void incrementThornsStat() {
+        stats.put(THORNS_STAT, stats.getOrDefault(THORNS_STAT, 0) + THORNS);
     }
 }
