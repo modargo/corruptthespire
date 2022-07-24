f5d5d1acb01336eb71e6837b788e2fe6981dc583
modargo <modargo@gmail.com>
2022-07-23 22:55:21 -0400

    Add relic stats for Bottomless Mead Horn

--- src/main/java/corruptthespire/relics/corrupted/BottomlessMeadHorn.java
+++ src/main/java/corruptthespire/relics/corrupted/BottomlessMeadHorn.java
@@ -1,6 +1,9 @@
 package corruptthespire.relics.corrupted;
 
 import com.badlogic.gdx.graphics.Texture;
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.actions.common.ExhaustSpecificCardAction;
 import com.megacrit.cardcrawl.actions.common.RelicAboveCreatureAction;
@@ -14,7 +17,9 @@ import corruptthespire.util.TextureLoader;
 
 import java.text.MessageFormat;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class BottomlessMeadHorn extends AbstractCorruptedRelic {
     public static final String ID = "CorruptTheSpire:BottomlessMeadHorn";
@@ -27,6 +32,11 @@ public class BottomlessMeadHorn extends AbstractCorruptedRelic {
     private static final int EXHAUST = 1;
     private static final int DRAW = 1;
 
+    private static final Map<String, Integer> stats = new HashMap<>();
+    private static final String FIRST_STAT = "first";
+    private static final String SECOND_STAT = "second";
+    private static final String THIRD_STAT = "third";
+
     public BottomlessMeadHorn() {
         super(ID, IMG, OUTLINE, RelicTier.SPECIAL, LandingSound.CLINK);
     }
@@ -35,6 +45,7 @@ public class BottomlessMeadHorn extends AbstractCorruptedRelic {
     public void atBattleStartPreDraw() {
         int deckSize = AbstractDungeon.player.masterDeck.size();
         if (deckSize >= THRESHOLD_2) {
+            incrementSecondStat();
             List<AbstractCard> strikes = new ArrayList<>();
             List<AbstractCard> unupgradedStrikes = new ArrayList<>();
             List<AbstractCard> defends = new ArrayList<>();
@@ -69,6 +80,7 @@ public class BottomlessMeadHorn extends AbstractCorruptedRelic {
         }
 
         if (deckSize >= THRESHOLD_3) {
+            incrementThirdStat();
             AbstractDungeon.player.gameHandSize += DRAW;
         }
     }
@@ -81,6 +93,7 @@ public class BottomlessMeadHorn extends AbstractCorruptedRelic {
     @Override
     public void atBattleStart() {
         if (AbstractDungeon.player.masterDeck.size() >= THRESHOLD_1) {
+            incrementFirstStat();
             this.flash();
             this.addToTop(new ApplyPowerAction(AbstractDungeon.player, AbstractDungeon.player, new StrengthPower(AbstractDungeon.player, STATS)));
             this.addToTop(new ApplyPowerAction(AbstractDungeon.player, AbstractDungeon.player, new DexterityPower(AbstractDungeon.player, STATS)));
@@ -96,5 +109,51 @@ public class BottomlessMeadHorn extends AbstractCorruptedRelic {
     @Override
     public AbstractRelic makeCopy() {
         return new BottomlessMeadHorn();
+    }
+
+    public String getStatsDescription() {
+        return MessageFormat.format(DESCRIPTIONS[1], THRESHOLD_1, stats.get(FIRST_STAT), THRESHOLD_2, stats.get(SECOND_STAT), THRESHOLD_3, stats.get(THIRD_STAT));
+    }
+
+    public String getExtendedStatsDescription(int totalCombats, int totalTurns) {
+        return getStatsDescription();
+    }
+
+    public void resetStats() {
+        stats.put(FIRST_STAT, 0);
+        stats.put(SECOND_STAT, 0);
+        stats.put(THIRD_STAT, 0);
+    }
+
+    public JsonElement onSaveStats() {
+        Gson gson = new Gson();
+        List<Integer> statsToSave = new ArrayList<>();
+        statsToSave.add(stats.get(FIRST_STAT));
+        statsToSave.add(stats.get(SECOND_STAT));
+        statsToSave.add(stats.get(THIRD_STAT));
+        return gson.toJsonTree(statsToSave);
+    }
+
+    public void onLoadStats(JsonElement jsonElement) {
+        if (jsonElement != null) {
+            JsonArray jsonArray = jsonElement.getAsJsonArray();
+            stats.put(FIRST_STAT, jsonArray.get(0).getAsInt());
+            stats.put(SECOND_STAT, jsonArray.get(1).getAsInt());
+            stats.put(THIRD_STAT, jsonArray.get(2).getAsInt());
+        } else {
+            resetStats();
+        }
+    }
+
+    public static void incrementFirstStat() {
+        stats.put(FIRST_STAT, stats.getOrDefault(FIRST_STAT, 0) + 1);
+    }
+
+    public static void incrementSecondStat() {
+        stats.put(SECOND_STAT, stats.getOrDefault(SECOND_STAT, 0) + 1);
+    }
+
+    public static void incrementThirdStat() {
+        stats.put(THIRD_STAT, stats.getOrDefault(THIRD_STAT, 0) + 1);
     }
 }
