f51b593dd7417867503f672e3d5d0c59a144bb3e
modargo <modargo@gmail.com>
2022-10-02 09:52:15 -0400

    Remove random upgrade per floor debug code

--- src/main/java/corruptthespire/savables/logs/RandomUpgradeRewardPerFloorLog.java
+++ src/main/java/corruptthespire/savables/logs/RandomUpgradeRewardPerFloorLog.java
@@ -1,15 +1,10 @@
 package corruptthespire.savables.logs;
 
 import basemod.abstracts.CustomSavable;
-import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class RandomUpgradeRewardPerFloorLog implements CustomSavable<List<List<String>>> {
-    private static final Logger logger = LogManager.getLogger(RandomUpgradeRewardPerFloorLog.class.getName());
     public static final String SaveKey = "RandomUpgradeRewardPerFloor";
     public static List<List<String>> randomUpgradeRewardPerFloorLog;
 
@@ -21,13 +16,5 @@ public class RandomUpgradeRewardPerFloorLog implements CustomSavable<List<List<S
     @Override
     public void onLoad(List<List<String>> randomUpgradeRewardLog) {
         RandomUpgradeRewardPerFloorLog.randomUpgradeRewardPerFloorLog = randomUpgradeRewardLog;
-        logger.info("AbstractDungeon.floorNum: " + AbstractDungeon.floorNum);
-        if (randomUpgradeRewardLog == null) {
-            logger.info("Specially initializing new log list, remove this before pushing");
-            RandomUpgradeRewardPerFloorLog.randomUpgradeRewardPerFloorLog = new ArrayList<>();
-            for (int i = 0; i < 4; i++) {
-                RandomUpgradeRewardPerFloorLog.randomUpgradeRewardPerFloorLog.add(new ArrayList<>());
-            }
-        }
     }
 }
