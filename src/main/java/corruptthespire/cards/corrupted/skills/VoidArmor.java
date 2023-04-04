00a4231b71c86827d8ffa1dbb76d71984475a5e6
modargo <modargo@gmail.com>
2023-04-04 02:42:45 -0400

    Increase Void Armor block by 1

--- src/main/java/corruptthespire/cards/corrupted/skills/VoidArmor.java
+++ src/main/java/corruptthespire/cards/corrupted/skills/VoidArmor.java
@@ -20,7 +20,7 @@ public class VoidArmor extends AbstractCorruptedCard {
     public static final String NAME = cardStrings.NAME;
     public static final String DESCRIPTION = cardStrings.DESCRIPTION;
     private static final int COST = 2;
-    private static final int BLOCK = 18;
+    private static final int BLOCK = 19;
     private static final int UPGRADE_BLOCK = 5;
     private static final int WEAK = 1;
     private static final int UPGRADE_WEAK = 1;
