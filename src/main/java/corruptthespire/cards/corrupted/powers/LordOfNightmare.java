897374bea97d6d85dcdfc4e08d6628de2acd1155
modargo <modargo@gmail.com>
2023-04-12 11:58:02 -0400

    Reduce Lord of Nightmare cost to 1

--- src/main/java/corruptthespire/cards/corrupted/powers/LordOfNightmare.java
+++ src/main/java/corruptthespire/cards/corrupted/powers/LordOfNightmare.java
@@ -16,7 +16,7 @@ public class LordOfNightmare extends AbstractCorruptedCard {
     private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
     public static final String NAME = cardStrings.NAME;
     public static final String DESCRIPTION = cardStrings.DESCRIPTION;
-    private static final int COST = 2;
+    private static final int COST = 1;
     private static final int UPGRADE_COST = 1;
     private static final int DRAW = 1;
     private static final int BLOCK = 6;
